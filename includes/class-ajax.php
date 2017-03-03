7413a39ee4357626175e27cfdc0f4923baf27f09
higorchristianfe@gmail.com <higorchristianfe@gmail.com>
2017-03-03 10:26:39 -0300

    Class para processar requisições http via ajax

--- includes/class-ajax.php
+++ includes/class-ajax.php
@@ -7,6 +7,11 @@ endif;
 class Ajax {
 
     public function __construct() {
+        add_action('wp_ajax_save_tell_a_friend', array($this, 'action_save_tell_a_friend'));
+        add_action('wp_ajax_nopriv_save_tell_a_friend', array($this, 'action_save_tell_a_friend'));
+    }
+
+    public function action_save_tell_a_friend() {
         
     }
 
