aec35d1276dc9d4345700a42adc5f237dab26745
higorchristianfe@gmail.com <higorchristianfe@gmail.com>
2017-03-04 22:07:44 -0300

    Template list friend

--- includes/admin-templates/tell-a-friend.php
+++ includes/admin-templates/tell-a-friend.php
@@ -1,1 +1,20 @@
-<h1>Olá, mundo!!!</h1>
+<?php
+// Exit if accessed directly.
+if (!defined('ABSPATH')) {
+    exit;
+}
+?>
+
+<div class="wrap">
+
+    <h2>
+        <?php echo esc_html(get_admin_page_title()); ?>
+    </h2>
+
+    <div style="background-color: #fff; padding: 10px; margin-top: 20px;">
+
+
+
+    </div><!-- /.table-list -->
+
+</div><!-- /.wrap -->
