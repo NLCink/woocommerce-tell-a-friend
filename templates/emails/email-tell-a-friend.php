1af6ce286ffd4a020b6865898c495f588cfd696d
higorchristianfe@gmail.com <higorchristianfe@gmail.com>
2017-03-06 10:32:22 -0300

    Template email para o amigo indicado

---
+++ templates/emails/email-tell-a-friend.php
@@ -0,0 +1,43 @@
+<?php
+if (!defined('ABSPATH')) {
+    exit;
+}
+?>
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta http-equiv="Content-Type" content="text/html; charset=<?php bloginfo('charset'); ?>" />
+        <title><?php echo get_bloginfo('name', 'display'); ?></title>
+    </head>
+    <body style="background-color: #eee;">
+
+        <div style="width: 600px; display: table; margin: 0 auto;">
+
+            <?php
+            if (get_option('taf_thumb_header', false) != false):
+                ?>
+                <div style="width: 100%; padding: 10px; background-color:#5e1e14; ">
+                    <a target="_blank" href="<?php echo get_site_url(); ?>">
+                        <img src="<?php echo get_option('taf_thumb_header'); ?>">
+                    </a>
+                </div>
+                <?php
+            endif;
+            ?>
+
+            <div style="width: 100%; background-color: #fff; padding: 10px;">
+                <?php
+                echo get_option('taf_friend_email');
+                ?>
+            </div>
+
+            <div style="width: 100%; background-color: #f8f8f8; padding: 10px; text-align: center; border-top: dotted 1px #ccc;">
+                <a target="_blank" style="color: #5e1e14; font-size: 0.750em;" href="<?php echo get_site_url(); ?>">
+                    <?php echo bloginfo('site-name'); ?>
+                </a>
+            </div>
+
+        </div>
+
+    </body>
+</html>
