b324abfa0925e4b6779942255db234a46a769e72
higorchristianfe@gmail.com <higorchristianfe@gmail.com>
2017-03-08 14:24:16 -0300

    functions plugin

--- includes/functions.php
+++ includes/functions.php
@@ -16,10 +16,31 @@ if (!function_exists('woo_plugin_dir_url')):
 
 endif;
 
-if (!function_exists('woo_file')):
+if (!function_exists('tell_a_frind_replace_string_template')):
 
-    function woo_file() {
-        return __FILE__;
+    /**
+     * Replace especial tags templates
+     * 
+     * $str_array = array(
+     *      '{friend_cupom}' => 'cupom_123',
+     *      '{validade_cupom}' => '05/04/2017',
+     *      '{valor_desconto}' => '10%',
+     *      '{email_friend}' => 'higor@gmail.com',
+     *      '{email_my}' => 'maira@gmail.com'
+     * );
+     * 
+     * @param array $str_array
+     * @param mixed $template
+     * @return mixed
+     */
+    function replace_especial_tags_templates(array $str_array, $template) {
+
+        ob_start();
+        include $template;
+        $output_prev = ob_get_clean();
+
+        return str_replace(array_keys($str_array), array_values($str_array), $output_prev);
+ 
     }
 
 endif;
