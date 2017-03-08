2b12532581debd107aa141890921d3b656e69768
higorchristianfe@gmail.com <higorchristianfe@gmail.com>
2017-03-08 14:20:58 -0300

    Admin script

--- assets/js/admin-script.js
+++ assets/js/admin-script.js
@@ -2,4 +2,43 @@
 
     $(document).taf_image_header_manager();
 
+    $("#table-tell-a-friend").DataTable({
+        "responsive": true,
+        "language": {
+            "sEmptyTable": "Nenhum registro encontrado",
+            "sInfo": "Mostrando de _START_ até _END_ de _TOTAL_ registros",
+            "sInfoEmpty": "Mostrando 0 até 0 de 0 registros",
+            "sInfoFiltered": "(Filtrados de _MAX_ registros)",
+            "sInfoPostFix": "",
+            "sInfoThousands": ".",
+            "sLengthMenu": "_MENU_ Por Página",
+            "sLoadingRecords": "Carregando...",
+            "sProcessing": "Processando...",
+            "sZeroRecords": "Nenhum registro encontrado",
+            "sSearch": "Pesquisar",
+            "oPaginate": {
+                "sNext": "Próximo",
+                "sPrevious": "Anterior",
+                "sFirst": "Primeiro",
+                "sLast": "Último"
+            },
+            "oAria": {
+                "sSortAscending": ": Ordenar colunas de forma ascendente",
+                "sSortDescending": ": Ordenar colunas de forma descendente"
+            }
+        },
+        "processing": true,
+        "serverSide": true,
+        "ajax": {
+            'url': t_a_f_obj.t_a_f_ajax_url,
+            'type': 'POST',
+            'data': {
+                'action': 'list_tell_a_friend'
+            }
+        },
+        columnDefs: [
+            {className: "dt-align-center", "targets": [2, 3, 4]}
+        ]
+    });
+
 })(jQuery);
